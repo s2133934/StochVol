83032f52d87b1ea72880ed7109239a0e8c5e0a4b
s2133934 <s2133934@sms.ed.ac.uk>
2021-08-17 14:44:31 +0100

    added R notebooks for SABR, CKLS, Heston

--- optimisation_tests.m
+++ optimisation_tests.m
@@ -2,9 +2,13 @@
 clear all; clc
 
 % fun = @(x)100*(x(2) - x(1)^2)^2 + (1 - x(1))^2;
-% x0 = [-1.2,1];
-% x = fminsearch(fun,x0)
-
+f = @(x,a)100*(x(2) - x(1)^2)^2 + (a-x(1))^2;
+t0 = [-1,1.9];
+a=3;
+fun = @(x)f(x,a);
+options = optimset('PlotFcns',@optimplotfval);
+[x,fval] = fminsearch(fun,t0,options)
+%%
 %%% Generate path:
 %generate timebase
 x_0 = 0.01;
@@ -36,25 +40,36 @@ end
 plot(t_grid,X) %Shows the OU path generated
 
 x_excel=[0.01, 0.01178935, 0.013438872, 0.008729023, 0.011549686, 0.009860681, 0.01256937, 0.009693261, 0.01543231, 0.014349636, 0.011756408, 0.010903393, 0.00650562, -0.001079047, -0.003765747];
-X=x_excel;
-N=length(x_excel);
-dt=0.001;
+% X=x_excel;
+% N=length(x_excel);
+% dt=0.001;
 
 %%% Write the MLE for OU
-alpha = 0.01;
-gammma = 0;
-beta = -0.1;
-sigma = 0.1;
-summ=0;
+% alpha = 0.01;
+% gammma = 0;
+% beta = -0.1;
+% sigma = 0.1;
+% summ=0;
 
+%optimise!
+new_call = multiplying(10,20)
+newer_call = multiplying(5,10)
+
+mle_val = ckls_mle(X, 0.01,-0.1,0,0.1,0.001)
+
+function res = ckls_mle(X,alpha, beta, gammma, sigma, dt)
+N=length(X);
+res=0;
 for j=2:N
     term0 = (sqrt(2 * sigma ^ 2 * pi * dt) * (abs(X(j - 1)) ^ (gammma)));
     term1 = (2 * sigma ^ 2) * dt * (abs(X(j - 1)) ^ (2 * gammma));
     term2 = (X(j) - (X(j - 1) * (1 + beta * dt) + alpha * dt)) ^ 2;
-    summ = summ + log(1 / term0) - (term2 * (1 / term1));
+    res = res + log(1 / term0) - (term2 * (1 / term1));
+end 
 end
 
-summ 
 
-%optimise!
+function res = multiplying(a,b)
+res = a*b;
+end
 
